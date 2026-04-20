217358a977b0168aa18db949cb499268637ca5f5
irajvaibhav <raj30.vaibhav@gmail.com>
2026-04-20 17:20:32 +0530

    Add Jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
